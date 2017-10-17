d7d2c63c3d493aa52c2d4e11a8f68fcbd1bbb750
JunghoonKong <rhdtl78@gmail.com>
2017-10-17 17:19:09 +0900

    index.html init. firebase initiate

---
+++ js/fireinit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDnzKEvZg6NiGEb3lroB6VEjFZNP_2VVKk",
+  authDomain: "pleasereadbook.firebaseapp.com",
+  databaseURL: "https://pleasereadbook.firebaseio.com",
+  projectId: "pleasereadbook",
+  storageBucket: "",
+  messagingSenderId: "291094516014"
+};
+firebase.initializeApp(config);
