483c5189f9db6aed6e4ca82274d27e79f13a974f
백승윤 <BaekSeungYun@baegjiyun-ui-MacBook-Air.local>
2017-10-24 15:10:46 +0900

    fix

--- js/fireinit.js
+++ js/fireinit.js
@@ -1,15 +1,4 @@
 // Initialize Firebase
-<<<<<<< HEAD
-var config = {
-  apiKey: "AIzaSyDnzKEvZg6NiGEb3lroB6VEjFZNP_2VVKk",
-  authDomain: "pleasereadbook.firebaseapp.com",
-  databaseURL: "https://pleasereadbook.firebaseio.com",
-  projectId: "pleasereadbook",
-  storageBucket: "pleasereadbook.appspot.com",
-  messagingSenderId: "291094516014"
-};
-firebase.initializeApp(config);
-=======
   var config = {
     apiKey: "AIzaSyDnzKEvZg6NiGEb3lroB6VEjFZNP_2VVKk",
     authDomain: "pleasereadbook.firebaseapp.com",
@@ -19,4 +8,3 @@ firebase.initializeApp(config);
     messagingSenderId: "291094516014"
   };
   firebase.initializeApp(config);
->>>>>>> 47139ab7e8de6bb4fa03d1708f17886007b3e4dd
