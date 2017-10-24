331bd7bb01031a2ec61db896ce1532f4047b9bca
Leeyounho <nasopida@naver.com>
2017-10-24 14:48:07 +0900

    정렬완료한부분 다시 커밋

---
+++ js/sortByRate.js
@@ -0,0 +1,41 @@
+var array_length;
+function heap_root(input, i) {
+  var left = 2 * i + 1;
+  var right = 2 * i + 2;
+  var max = i;
+
+  if ((left < array_length) && (input[left].rate > input[max].rate)) {
+    max = left;
+  }
+
+  if ((right < array_length) && (input[right].rate > input[max].rate)) {
+    max = right;
+  }
+
+  if (max != i) {
+    swap(input, i, max);
+    heap_root(input, max);
+  }
+}
+
+function swap(input, index_A, index_B) {
+  var temp = input[index_A];
+
+  input[index_A] = input[index_B];
+  input[index_B] = temp;
+}
+
+function heapSort(input) {
+
+  array_length = input.length;
+
+  for (var i = Math.floor(array_length / 2); i >= 0; i -= 1) {
+    heap_root(input, i);
+  }
+
+  for (i = input.length - 1; i > 0; i--) {
+    swap(input, 0, i);
+    array_length--;
+    heap_root(input, 0);
+  }
+}
