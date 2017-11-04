29b393d9d3f24fb67e3d3268d31cd05b98b40bfd
정준석 <joon123suk@gmail.com>
2017-11-04 15:59:29 +0900

    </tr> 태그 빠진 것 수정, 주석 추가

--- js/myclip.js
+++ js/myclip.js
@@ -5,6 +5,7 @@ $(function ($) {
     var $els = $("tr input[type='checkbox']:checked");
     $els.each(function(idx, el) {
       //TODO: 책의 키값 유저정보에 추가
+      
     });
     resettab();
   });
@@ -19,6 +20,7 @@ $(function ($) {
     $('.end').click(function(){ // 읽기 완료 버튼
       $(this).empty().text("독서 완료됨").removeClass("btn-success end").addClass("disabled"); //'독서 완료됨' 버튼 대신 버튼을 없애고 '독파시간: 00일 00시간' 으로 표시하는 것도 고려
       $(this).parents("tr").find(".date-ended").text(d.getFullYear() + '.' + (d.getMonth()+1) + '.' + d.getDate());
+      //TODO: 읽기 완료 시 평점 선택 기록
     });
   });
   $('#del').click(function() { // 도서 삭제 버튼
@@ -68,7 +70,7 @@ $('#add').click(function(){ // 책 선택창 (팝업) 띄우는 버튼
 });
 
 
-//TODO: /users/book 에서 책의 키를 가져와 /book에서 비교해 같은 것들을 목록에 표시
+//TODO: /users/(선택한 책목록) 에서 책의 키를 가져와 /book에서 비교해 같은 것들을 목록에 표시
 //      책의 읽기 상태도 불러와야 함 (YYYY.MM.DD 에 읽기 시작)
 $(function resettab ($) { //선택해놓은 책 목록 새로고침하는 함수
   $('#tab > tbody > *').remove();
@@ -78,7 +80,7 @@ $(function resettab ($) { //선택해놓은 책 목록 새로고침하는 함수
   var ary = [];
   var book = {};
   var numBooks = 0;
-  firebase.database().ref('/users').on('value', function(snapshot) { //여기 수정필요
+  firebase.database().ref('/users').on('value', function(snapshot) { //여기 ref 경로 수정필요
     snapshot.forEach(function(childsnapshot) {
       firebase.database().ref('/book/' + childsnapshot.key).on('value', function(book) {
         bookTitle = book.val().title;
