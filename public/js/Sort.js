e2c0d39f3b1a0406ba7c9b6729f04ed298cdae6b
junghoonK <rhdtl78@gmail.com>
2017-12-04 20:31:42 +0900

    Sort.js 수정
    정렬이 제대로 되지 않는 버그 수정

--- public/js/Sort.js
+++ public/js/Sort.js
@@ -1,34 +1,21 @@
 var array_length;
-function Sort(rule, input) {
-  array_length = input.length;
-
-  for (var i = Math.floor(array_length / 2); i >= 0; i -= 1) {
-    heapRoot(input, i);
-  }
-
-  for (i = input.length - 1; i > 0; i--) {
-    swap(input, 0, i);
-    array_length--;
-    heapRoot(input, 0, rule);
-  }
-
-}
-function heapRoot(input, i, rule) {
+/* to create MAX  array */
+function heap_root(input, i, rule) {
   var left = 2 * i + 1;
   var right = 2 * i + 2;
   var max = i;
 
-  if ((left < array_length) && (input[left][rule] < input[max][rule])) {
-    max = left;
+  if (left < array_length && input[left][rule] < input[max][rule]) {
+      max = left;
   }
 
-  if ((right < array_length) && (input[right][rule] < input[max][rule])) {
-    max = right;
+  if (right < array_length && input[right][rule] < input[max][rule])     {
+      max = right;
   }
 
   if (max != i) {
-    swap(input, i, max);
-    heapRoot(input, max);
+      swap(input, i, max);
+      heap_root(input, max, rule);
   }
 }
 
@@ -37,4 +24,21 @@ function swap(input, index_A, index_B) {
 
   input[index_A] = input[index_B];
   input[index_B] = temp;
+}
+
+function Sort(rule, input) {
+
+  array_length = input.length;
+
+  for (var i = Math.floor(array_length / 2); i >= 0; i -= 1)      {
+      heap_root(input, i, rule);
+    }
+
+  for (i = input.length - 1; i > 0; i--) {
+      swap(input, 0, i);
+      array_length--;
+
+
+      heap_root(input, 0, rule);
+  }
 }
